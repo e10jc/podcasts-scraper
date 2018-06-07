ca0650988e4e4c16bcafbbfa56c53d83a8c640c3
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-06 19:52:20 -0700

    back to single file

--- scrape-categories.js
+++ scrape-categories.js
@@ -1,5 +1,5 @@
+const Inserter = require('./inserter')
 const scrape = require('./scrape')
-const scrapeCategory = require('./scrape-category')
 
 const scrapeCategories = async () => {
   const {categories} = await scrape({
@@ -19,8 +19,106 @@ const scrapeCategories = async () => {
   return categories
 }
 
-(async () => {
-  const categories = await scrapeCategories()
+const scrapeLetters = async (category) => {
+  const {letters} = await scrape({
+    opts: {
+      letters: {
+        listItem: 'ul.alpha li a',
+        data: {
+          url: {
+            attr: 'href',
+          }
+        }
+      }
+    },
+    url: category.url,
+  })
+  return letters
+}
 
-  await Promise.all(categories.map(c => scrapeCategory(c)))
+const scrapePages = async (letter) => {
+  const {pages} = await scrape({
+    opts: {
+      pages: {
+        listItem: 'ul.paginate li a',
+        data: {
+          url: {
+            attr: 'href',
+          }
+        }
+      }
+    },
+    url: letter.url,
+  })
+  return pages
+}
+
+const scrapePodcasts = async (page) => {
+  const {podcasts} = await scrape({
+    opts: {
+      podcasts: {
+        listItem: '#selectedcontent ul li a',
+        data: {
+          title: '',
+          url: {
+            attr: 'href'
+          }
+        }
+      }
+    },
+    url: page.url,
+  })
+  return podcasts
+}
+
+const scrapePodcast = async (podcast) => {
+  const details = await scrape({
+    opts: {
+      id: {
+        attr: 'content',
+        convert: (html) => parseInt(html, 10),
+        selector: 'meta[name="apple:content_id"]',
+      },
+      reviewsAvg: {
+        convert: (html) => {
+          if (html) {
+            return parseFloat(html)
+          }
+        },
+        selector: 'span[itemprop="ratingValue"]'
+      },
+      reviewsCnt: {
+        convert: (html) => {
+          if (html) {
+            return parseInt(html.split(' ')[0], 10)
+          }
+        },
+        selector: '.rating-count',
+      },
+    },
+    url: podcast.url,
+  })
+  return details
+}
+
+(async () => {
+  const inserter = new Inserter()
+
+  for (const category of await scrapeCategories()) {
+    for (const letter of await scrapeLetters(category)) {
+      for (const page of await scrapePages(letter)) {
+        for (const podcast of await scrapePodcasts(page)) {
+          const row = {
+            category: category.title,
+            title: podcast.title,
+            url: podcast.url,
+            ...await scrapePodcast(podcast)
+          }
+          await inserter.push(row)
+        }
+      }
+    }
+  }
+
+  await inserter.insert()
 })()
