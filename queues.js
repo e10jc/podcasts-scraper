1f8ca45438ca36c9ae17822bcd7ce07145cde0f3
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-08 07:31:26 -0700

    npm-run-all instead of pm2

--- queues.js
+++ queues.js
@@ -1,9 +1,10 @@
 const Queue = require('bee-queue')
 
-const categoriesQueue = new Queue('categories')
-const lettersQueue = new Queue('letters')
-const pagesQueue = new Queue('pages')
-const podcastsQueue = new Queue('podcasts')
-const detailsQueue = new Queue('details')
+const settings = {redis: {host: process.env.REDIS_HOST}}
+const categoriesQueue = new Queue('categories', settings)
+const lettersQueue = new Queue('letters', settings)
+const pagesQueue = new Queue('pages', settings)
+const podcastsQueue = new Queue('podcasts', settings)
+const detailsQueue = new Queue('details', settings)
 
 module.exports = {categoriesQueue, lettersQueue, pagesQueue, podcastsQueue, detailsQueue}
