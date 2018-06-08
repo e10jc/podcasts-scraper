184e198d84611097989b19f90b3d864f0f1acb1a
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-08 10:21:23 -0700

    progress bar, 4 podcast processors

--- process-podcasts.js
+++ process-podcasts.js
@@ -1,8 +1,11 @@
+const ProgressBar = require('progress')
+
 const {shuffle} = require('./helpers')
 const {podcastsQueue} = require('./queues')
 const {scrapePodcast} = require('./scrape')
 
 const inserter = require('./inserter')
+const progressBar = new ProgressBar(':current inserts in :elapseds, last: :title', {total: Number.MAX_SAFE_INTEGER})
 
 podcastsQueue.process(async ({data: {category, podcast}}) => {
   const details = await scrapePodcast(podcast)
@@ -13,5 +16,6 @@ podcastsQueue.process(async ({data: {category, podcast}}) => {
     ...details
   }
   await inserter.push(row)
+  progressBar.tick({title: `${category.title} - ${podcast.title}`})
   // console.log(`Inserted: ${category.title} - ${podcast.title}`)
 })
