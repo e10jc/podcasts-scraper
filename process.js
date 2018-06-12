4fcf5af1123e4e0ca1761b7144ae533e4ec31f4e
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-12 07:32:08 -0700

    optional Progress

--- process.js
+++ process.js
@@ -12,7 +12,7 @@ const {
 const Inserter = require('./inserter')
 const inserter = new Inserter()
 
-const progressBar = new ProgressBar(':letters letters, :pages pages, :podcasts podcasts, :details details, :memory MB used', {total: Number.MAX_SAFE_INTEGER})
+const progressBar = ProgressBar && new ProgressBar(':letters letters, :pages pages, :podcasts podcasts, :details details, :memory MB used, :rate per sec', {total: Number.MAX_SAFE_INTEGER})
 const ticks = {letters: 0, pages: 0, podcasts: 0, details: 0}
 
 queue.process(25, async ({data: {category, letter, page, podcast}}) => {
@@ -29,28 +29,27 @@ queue.process(25, async ({data: {category, letter, page, podcast}}) => {
     ++ticks.details
   } else if (page) {
     for (const podcast of shuffle(await scrapePodcasts(page))) {
-      const job = queue.createJob({category, podcast})
-      job.save()
+      await queue.createJob({category, podcast}).save()
       // console.log(`Queued: ${category.title} - ${podcast.title}`)
       ++ticks.podcasts
     }
   } else if (letter) {
     for (const page of shuffle(await scrapePages(letter))) {
-      const job = queue.createJob({category, page})
-      job.save()
+      await queue.createJob({category, page}).save()
       // console.log(`Queued: ${page.url}`)
       ++ticks.pages
     }
 
   } else if (category) {
     for (const letter of shuffle(await scrapeLetters(category))) {
-      const job = queue.createJob({category, letter})
-      job.save()
+      await queue.createJob({category, letter}).save()
       // console.log(`Queued: ${letter.url}`)
       ++ticks.letters
     }
   }
 
-  ticks.memory = process.memoryUsage().heapUsed / 1024 / 1024
-  progressBar.tick(ticks)
+  if (ProgressBar) {
+    ticks.memory = parseInt(process.memoryUsage().heapUsed / 1024 / 1024, 10)
+    progressBar.tick(ticks)
+  }
 })
--- queue.js
+++ queue.js
@@ -1,9 +1,11 @@
 const Queue = require('bee-queue')
 
 const queue = new Queue('processor', {
+  getEvents: false,
   redis: {host: process.env.REDIS_HOST},
   removeOnFailure: true,
   removeOnSuccess: true,
+  sendEvents: false,
   storeJobs: false,
 })
 
