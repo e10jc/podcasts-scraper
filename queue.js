08231e4ec19bfcbc62de974b6bff102109ec25d0
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-18 11:49:38 -0700

    no store jobs

--- queue.js
+++ queue.js
@@ -3,21 +3,22 @@ const Queue = require('bee-queue')
 const queue = new Queue('processor', {
   redis: {host: process.env.REDIS_HOST},
   stallInterval: 60000,
+  storeJobs: false,
 })
 
 queue.on('error', (err) => {
   console.error(`Queue error: ${err.message}`)
 })
 
-queue.on('retrying', (job, err) => {
+queue.on('retrying', (_, err) => {
   console.error(`Queue retrying: ${err.message}`)
 })
 
-queue.on('stalled', (jobId) => {
-  console.error(`Queue stalled on job ID ${jobId}!`)
+queue.on('stalled', () => {
+  console.error(`Queue stalled!`)
 })
 
-queue.on('failed', (job, err) => {
+queue.on('failed', (_, err) => {
   console.error(`Queue failure: ${err.message}`)
 })
 
