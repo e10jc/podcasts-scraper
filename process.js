0375814e391e630d94c4d438665a75f3d22a9df0
Ethan Jon <e10jc@users.noreply.github.com>
2018-06-19 11:58:16 -0700

    try catch process

--- process.js
+++ process.js
@@ -10,24 +10,28 @@ const {
 const Inserter = require('./inserter')
 const inserter = new Inserter()
 
-queue.process(5, async ({data: {category, podcast}}) => {
-  if (podcast) {
-    const details = await scrapePodcast(podcast)
-    const row = {
-      category: category.title,
-      title: podcast.title,
-      url: podcast.url,
-      ...details
-    }
-    await inserter.push(row)
-  } else if (category) {
-    console.log(`${new Date()} Scraping ${category.title}...`)
-    for (const letter of shuffle(await scrapeLetters(category))) {
-      for (const page of shuffle(await scrapePages(letter))) {
-        for (const podcast of shuffle(await scrapePodcasts(page))) {
-          await queue.createJob({category, podcast}).save()
+queue.process(3, async ({data: {category, podcast}}) => {
+  try {
+    if (podcast) {
+      const details = await scrapePodcast(podcast)
+      const row = {
+        category: category.title,
+        title: podcast.title,
+        url: podcast.url,
+        ...details
+      }
+      await inserter.push(row)
+    } else if (category) {
+      console.log(`${new Date()} Scraping ${category.title}...`)
+      for (const letter of shuffle(await scrapeLetters(category))) {
+        for (const page of shuffle(await scrapePages(letter))) {
+          for (const podcast of shuffle(await scrapePodcasts(page))) {
+            await queue.createJob({category, podcast}).save()
+          }
         }
       }
     }
+  } catch (err) {
+    console.error(`Process error: ${err.message}`)
   }
 })
