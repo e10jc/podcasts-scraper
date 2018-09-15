bef4793fb1715f811834cca13390afb795a9db61
Ethan Jon <e10jc@users.noreply.github.com>
2018-09-15 14:49:42 -0700

    no syncrhonize

--- ormconfig.js
+++ ormconfig.js
@@ -8,7 +8,7 @@ module.exports = {
   'username': process.env.RDS_USERNAME,
   'password': process.env.RDS_PASSWORD,
   'database': process.env.RDS_DB_NAME,
-  'synchronize': true,
+  'synchronize': false,
   'logging': true,
   'entities': [`${dirPrefix}entities/**/*.${fileSuffix}`],
   'migrations': [`${dirPrefix}migrations/**/*.${fileSuffix}`],
